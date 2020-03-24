d2edc2fcb9a8155a77da7e5e9e337d00e4bbbae0
tusharchopra123 <tusharchopra123@gmail.com>
2020-03-24 19:50:02 +0530

    using port if available

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 
 const express = require('express')
 const path = require('path')
+const server_port = process.env.PORT || 3333
 
 const app = express();
 
@@ -10,4 +11,4 @@ app.use(express.urlencoded({extended: true}))
 app.use('/', express.static(path.join(__dirname, 'public')))
 app.use('/api', require('./routes/api').route)
 
-app.listen(2678, () => console.log('Server started at http://localhost:2678'))
+app.listen(server_port, () => console.log('Server started at http://localhost:2678'))
