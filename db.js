0973ea31730afc465be0c9edac86632e2176c122
tusharchopra123 <tusharchopra123@gmail.com>
2020-03-25 10:58:05 +0530

    new changes

--- db.js
+++ db.js
@@ -1,57 +1,36 @@
-const { Client } = require('pg');
-const client = new Client({
-  connectionString: process.env.DATABASE_URL,
-  ssl: true,
+const Sequelize = require('sequelize')
+
+const db = new Sequelize( 'demo', 'b32_25044634', 'Tushar9958@', {
+    host: 'sql305.byethost.com',
+    dialect: 'mysql',
+    pool: {
+        min: 0,
+        max: 5,
+    }
 });
-
-client.connect();
-
-client.query(`CREATE TABLE test1(
-    id int PRIMARY KEY AUTO_INCREMENNT,
-    name varchar(80),
-    age int,
-    city varchar(40)
-)`, (err, res) => {
-  if (err) throw err;
-    console.log(err)
-
-});
-
-function getAlldata(){
-    return new Promise(function(resolve,reject){
-        client.query(
-            `select*from test1`,
-            function(err,rows,cols){
-                if(err){
-                    reject(err)
-                }else {
-                    resolve(rows)
-                }
-
-            }
-        )
-
-    })
-}
-function addNewdata(name){
-    return new Promise(function(resolve,reject){
-        client.query(
-            `INSERT INTO persons (name,age,city)values(?,?,?)`,
-            [name,age,city],
-            function(err,results){
-                if(err){
-                    reject(err)
-                }else {
-                    resolve()
-                }
-
-            }
-        )
-
-    })
-}
-
+const User = db.define('users', {
+    id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        primaryKey: true
+    },
+    name: {
+        type: Sequelize.STRING,
+        allowNull: false
+    }},{
+    }
+)
+db
+  .authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch(err => {
+    console.error('Unable to connect to the database:', err);
+  });
+db.sync()
+    .then(() => console.log("Database has been synced"))
+    .catch((err) => console.error(err))
 exports = module.exports = {
-    getAlldata,
-    addNewdata
+    User
 }
--- routes/api.js
+++ routes/api.js
@@ -1,18 +1,4 @@
-const route = require('express').Router()
-//This route is on the
-const db = require('../db')
-route.get('https://deploysample5.herokuapp.com/persons',(req,res)=>{
-//send all the persons as an array
-    db.getAllPersons()
-        .then((persons)=> res.send(persons))
-        .catch((err)=> res.send({error: err}))
-})
-route.post('https://deploysample5.herokuapp.com/persons',(req,res)=>{
-//Add the new son detals
-    db.addNewPerson(req.body.name,req.body.age,req.body.city)
-        .then(()=>res.redirect('https://deploysample5.herokuapp.com/api/'))
-        .catch((err)=>res.send({error:err}))
-})
-exports  = module.exports = {
+const route = require('express').Router
+exports = module.exports={
     route
 }
