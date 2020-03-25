bff864a3d99eb373c3f9ad215898ec10095d1d49
tusharchopra123 <tusharchopra123@gmail.com>
2020-03-25 10:44:43 +0530

    changes

--- routes/api.js
+++ routes/api.js
@@ -1,16 +1,16 @@
 const route = require('express').Router()
 //This route is on the
 const db = require('../db')
-route.get('/persons',(req,res)=>{
+route.get('https://deploysample5.herokuapp.com/persons',(req,res)=>{
 //send all the persons as an array
     db.getAllPersons()
         .then((persons)=> res.send(persons))
         .catch((err)=> res.send({error: err}))
 })
-route.post('/persons',(req,res)=>{
+route.post('https://deploysample5.herokuapp.com/persons',(req,res)=>{
 //Add the new son detals
     db.addNewPerson(req.body.name,req.body.age,req.body.city)
-        .then(()=>res.redirect('/api/'))
+        .then(()=>res.redirect('https://deploysample5.herokuapp.com/api/'))
         .catch((err)=>res.send({error:err}))
 })
 exports  = module.exports = {
